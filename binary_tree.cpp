07aa0185f50144262f9469b7aa95794f5185af6e
Anastasiya Puzankova <ana@Anastasiyas-MacBook-Pro.local>
2021-06-16 01:53:39 +0300

    source added

---
+++ binary_tree.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Anastasiya Puzankova on 6/15/21.
+//
+
+#include "binary_tree.h"
---
+++ binary_tree.h
@@ -0,0 +1,14 @@
+//
+// Created by Anastasiya Puzankova on 6/15/21.
+//
+
+#ifndef TRAINING_TASKS_BINARY_TREE_H
+#define TRAINING_TASKS_BINARY_TREE_H
+
+
+class binary_tree {
+
+};
+
+
+#endif //TRAINING_TASKS_BINARY_TREE_H
